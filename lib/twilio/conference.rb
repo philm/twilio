2623e009b1bd588bb49e990ee3d7f52dbaa47336
Jonathan Rudenberg <jonathan@titanous.com>
2009-11-28 21:18:36 -0500

    conference REST API

---
+++ lib/twilio/conference.rb
@@ -0,0 +1,35 @@
+module Twilio
+    # The Conference REST resource allows you to query and manage the state of conferences.
+    # When a caller joins a conference via the Dial verb and Conference noun,
+    # a Conference Instance Resource is created to represent the conference room
+    # and a Participant Instance Resource is created to represent the caller who joined.
+  class Conference < TwilioObject
+    def list(optional = {})
+      Twilio.get("/Conferences", :query => optional)  
+    end
+    
+    def get(conference_sid)
+      Twilio.get("/Conferences/#{conference_sid}")  
+    end
+    
+    def participants(conference_sid, optional = {})
+      Twilio.get("/Conferences/#{conference_sid}/Participants", :query => optional) 
+    end
+    
+    def participant(conference_sid, call_sid)
+      Twilio.get("/Conferences/#{conference_sid}/Participants/#{call_sid}") 
+    end
+    
+    def mute_participant(conference_sid, call_sid)
+      Twilio.post("/Conferences/#{conference_sid}/Participants/#{call_sid}", :body => {:Muted => true})
+    end
+    
+    def unmute_participant(conference_sid, call_sid)
+      Twilio.post("/Conferences/#{conference_sid}/Participants/#{call_sid}", :body => {:Muted => false})
+    end
+    
+    def kick_participant(conference_sid, call_sid)
+      Twilio.delete("/Conferences/#{conference_sid}/Participants/#{call_sid}")
+    end
+  end
+end
---
+++ test/twilio/conference_test.rb
@@ -0,0 +1,46 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ConferenceTest < Test::Unit::TestCase #:nodoc: all
+  context "A conference" do
+    setup do
+      Twilio.connect('mysid', 'mytoken')
+    end
+    
+    should "be retrievable as a list" do
+      assert_equal stub_response(:get, :conferences, :resource => 'Conferences'), Twilio::Conference.list
+    end
+    
+    should "be retrievable individually" do
+      assert_equal stub_response(:get, :conference, :resource => 'Conferences/CFd0a50bbe038c437e87f6c82db8f37f21'), 
+        Twilio::Conference.get('CFd0a50bbe038c437e87f6c82db8f37f21')
+    end
+    
+    context "participant" do
+      should "be retrievable as a list" do
+        assert_equal stub_response(:get, :conference_participants, :resource => 'Conferences/CF9f2ead1ae43cdabeab102fa30d938378/Participants'),
+          Twilio::Conference.participants('CF9f2ead1ae43cdabeab102fa30d938378')
+      end
+      
+      should "be retrievable individually" do
+          assert_equal stub_response(:get, :conference_participant, :resource => 'Conferences/CF9f2ead1ae43cdabeab102fa30d938378/Participants/CA9ae8e040497c0598481c2031a154919e'),
+            Twilio::Conference.participant('CF9f2ead1ae43cdabeab102fa30d938378', 'CA9ae8e040497c0598481c2031a154919e')
+      end
+      
+      should "be muted" do
+        assert_equal stub_response(:post, :conference_participant_muted, :resource => 'Conferences/CF9f2ead1ae43cdabeab102fa30d938378/Participants/CA9ae8e040497c0598481c2031a154919e'),
+          Twilio::Conference.mute_participant('CF9f2ead1ae43cdabeab102fa30d938378', 'CA9ae8e040497c0598481c2031a154919e')
+      end
+      
+      should "be unmuted" do
+        assert_equal stub_response(:post, :conference_participant, :resource => 'Conferences/CF9f2ead1ae43cdabeab102fa30d938378/Participants/CA9ae8e040497c0598481c2031a154919e'),
+          Twilio::Conference.unmute_participant('CF9f2ead1ae43cdabeab102fa30d938378', 'CA9ae8e040497c0598481c2031a154919e')
+      end
+      
+      should "be kicked" do
+        stub_response(:delete, :conference_participant, :resource => 'Conferences/CF9f2ead1ae43cdabeab102fa30d938378/Participants/CA9ae8e040497c0598481c2031a154919e',
+                      :status => [ 204, "HTTPNoContent" ])
+        assert Twilio::Conference.kick_participant('CF9f2ead1ae43cdabeab102fa30d938378', 'CA9ae8e040497c0598481c2031a154919e')
+      end
+    end
+  end
+end
