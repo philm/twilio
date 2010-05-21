9e43ecf0d65afd1550e4898d70f5e55c895ed762
Jeff Wigal <jeff@wigaldesign.com>
2010-05-21 12:51:57 -0400

    add support for extended attributes on the local phone number

--- lib/twilio/local_phone_number.rb
+++ lib/twilio/local_phone_number.rb
@@ -5,13 +5,13 @@ module Twilio
   #   Twilio.connect('my_twilio_sid', 'my_auth_token')
   #   Twilio::LocalPhoneNumber.list
   class LocalPhoneNumber < TwilioObject
-    def create(url, area_code = nil, method = 'POST', friendly_name = nil)
+    def create(url, area_code = nil, method = 'POST', friendly_name = nil, options = {})
       Twilio.post("/IncomingPhoneNumbers/Local", :body => {
         :Url => url, 
         :AreaCode => area_code, 
         :Method => method,
         :FriendlyName => friendly_name 
-      })
+      }.merge(options))
     end
     
     def list
